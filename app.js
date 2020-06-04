c640499b4fd57a1e9daadc12691a31fa540f1620
Vitor Leonel <falecom@vitorleonel.com>
2020-06-04 15:07:41 -0300

    add headless env

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ const extractFunction = require("./functions/extract");
 
 puppeteer.use(StealthPlugin());
 
-puppeteer.launch({ headless: false }).then(async (browser) => {
-  const page = await browser.newPage();
+puppeteer
+  .launch({ headless: process.env.HEADLESS === "yes" })
+  .then(async (browser) => {
+    const page = await browser.newPage();
 
-  await page.goto("https://www.itau.com.br/empresas", {
-    waitUntil: "networkidle2",
+    await page.goto("https://www.itau.com.br/empresas", {
+      waitUntil: "networkidle2",
+    });
+
+    await authenticateFunction(page);
+    await extractFunction(page);
   });
-
-  await authenticateFunction(page);
-  await extractFunction(page);
-});
