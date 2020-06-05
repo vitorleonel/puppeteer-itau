52cfb081bb19683d361dfaf548393a420c803a25
Vitor Leonel <falecom@vitorleonel.com>
2020-06-05 16:03:30 -0300

    add beanstalkd service

---
+++ services/beanstalkd.js
@@ -0,0 +1,17 @@
+const BeanstalkdClient = require("beanstalkd");
+
+module.exports = {
+  send: async (payload) => {
+    const beanstalkd = new BeanstalkdClient.default(
+      process.env.BEANSTALKD_HOST,
+      process.env.BEANSTALKD_PORT
+    );
+
+    const client = await beanstalkd.connect();
+
+    await client.use(process.env.BEANSTALKD_QUEUE);
+    await client.put(0, 0, 1, JSON.stringify(payload));
+
+    client.quit();
+  },
+};
